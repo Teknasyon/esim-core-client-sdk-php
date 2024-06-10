9937966d1b680cd569fdc57f039cc9ed003ba3a8
Hakan Türksoy <hakanturksoy@teknasyon.com>
2024-06-10 16:51:22 +0300

    postSubscriberBalance added.

--- src/Service/eSIMCoreService.php
+++ src/Service/eSIMCoreService.php
@@ -10,6 +10,7 @@ use eSIM\eSIMCoreClient\Dto\Request\PackageDetailsByPackageCodeRequest;
 use eSIM\eSIMCoreClient\Dto\Request\PackageGroupsRequest;
 use eSIM\eSIMCoreClient\Dto\Request\PackagesByFootprintCodeRequest;
 use eSIM\eSIMCoreClient\Dto\Request\SignatureDto;
+use eSIM\eSIMCoreClient\Dto\Request\SubscriberBalanceRequest;
 use eSIM\eSIMCoreClient\Dto\Response\Order\BalanceDto;
 use eSIM\eSIMCoreClient\Dto\Response\Order\OrderDto;
 use eSIM\eSIMCoreClient\Dto\Response\Package\PackageDetailsDto;
@@ -41,6 +42,7 @@ class eSIMCoreService
     const CREATE_ORDER_ROUTE = '/order';
     const BALANCE_ROUTE = '/order/%s/balance';
     const ACTIVATE_ROUTE = '/order/%s/activate';
+    const SUBSCRIBER_BALANCE_ROUTE = '/subscriber/%s/balance';
     const CONTENT_TYPE = 'application/json';
 
     public function __construct(
@@ -261,6 +263,41 @@ class eSIMCoreService
                 sprintf(self::ACTIVATE_ROUTE, $activateOrderRequest->getTrackingNumber()),
                 [
                     'headers' => $headers
+                ]
+            );
+
+            return $response->getStatusCode() == 204;
+        } catch (ResourceNotFoundException|ClientExceptionInterface|DecodingExceptionInterface|RedirectionExceptionInterface|ServerExceptionInterface|TransportExceptionInterface $exception) {
+            throw new ClientException($exception->getMessage(), $exception->getCode());
+        }
+    }
+
+    /**
+     * @throws ClientException
+     */
+    public function postSubscriberBalance(SubscriberBalanceRequest $subscriberBalanceRequest): bool
+    {
+        try {
+            $headers = $this->getHeaders($subscriberBalanceRequest);
+
+            $payload = [
+                'transactionId' => $subscriberBalanceRequest->getTransactionId(),
+                'price' => $subscriberBalanceRequest->getPrice(),
+                'totalBalance' => $subscriberBalanceRequest->getTotalBalance(),
+            ];
+
+            $signatureDto = SignatureDto::builder()
+                ->setUrl($this->baseUri . self::SUBSCRIBER_BALANCE_ROUTE)
+                ->setHeaders($headers)
+                ->setPayload($payload);
+
+            $headers[Headers::SIGNATURE->value] = SignatureHelper::calculateSignature($signatureDto->toArray(), $this->secretKey);
+            $response = $this->eSIMCoreClient->request(
+                Request::METHOD_POST,
+                sprintf(self::SUBSCRIBER_BALANCE_ROUTE, $subscriberBalanceRequest->getOpaqueId()),
+                [
+                    'headers' => $headers,
+                    'json' => $payload
                 ]
             );
 
